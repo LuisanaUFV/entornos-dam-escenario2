1f6151a6b77a162ddd6b3e77e27dc98c7183f3af
DAM_PROFE <DAM_PROFE@DAM-00>
2023-01-16 09:38:41 +0100

    Segundo commit

--- src/ejemplo/Ejemplo.java
+++ src/ejemplo/Ejemplo.java
@@ -5,7 +5,9 @@ public class Ejemplo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hola mundo");
+		System.out.println("ADIOS mundo");
+		System.out.println("Mundo cruel");
+		
 	}
 
 }
